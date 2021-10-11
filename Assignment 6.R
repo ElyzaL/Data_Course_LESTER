461c586e7839f6f32932daf1111a0a6120987819
Elyza Lester <89542098+ElyzaL@users.noreply.github.com>
2021-10-11 13:28:00 -0600

    oct/11

--- Assignment 6.R
+++ Assignment 6.R
@@ -2,16 +2,46 @@ library(tidyverse)
 dat <- read_csv("~/Data_Course/Data/BioLog_Plate_Data.csv")
 dat
 
-
-
-# 1. Clean this data into tidy (long) form
+#clean 
+dat <- dat %>% 
+  pivot_longer(cols = starts_with("hr_"), 
+               names_to = "time", 
+               values_to = "absorbance", 
+               names_prefix = "hr_") %>% 
+  mutate(time= as.numeric(time)) %>% 
+  mutate(type= case_when('Sample_ID' == "CLear_Creek" ~ "water",
+                         'Sample_ID' == "Waste_Water" ~ "water",
+                         TRUE ~ "soil"))
+#graph 1
 dat %>% 
-  pivot_longer(cols = c("Hr_24", "Hr_48", "Hr_144"),
-               names_to = "absorbance",
-               values_to = "Time")
- 
+  filter(Dilution == 0.1) %>% 
+  ggplot(aes(x= time, y = absorbance)) +
+  geom_smooth(se =FALSE) +
+  facet_wrap(~ substrate) +
+  labs(subtitle = "Just dilution = 0.1", 
+       x= "Time",
+       y= "Absorbance",
+       color= "Type") +
+  theme_minimal()
 
 
+
+#graph 2
+df$Substrate %>% unique()
+
+p <- df %>% 
+  filter(substrate == "Itaonic Acid") %>% 
+  group_by(dilution, sample_id, time) %>% 
+  summarise(Mean_Absorbance = mean(absorbance), time) %>% 
+  ggplot(aes(x=time, y=Mean_Absorbance, color= sample_id)) +
+  geom_line()+
+  facet_wrap(~dilution)
+
+p+ transition_reveal(time)
+
+
+
+######### other try
 # 2. Creates a new column specifying whether 
 #      a sample is from soil or water
 dat$sample_type <- dat$`Sample ID`
